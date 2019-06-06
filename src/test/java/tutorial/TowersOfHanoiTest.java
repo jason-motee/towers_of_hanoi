9038dd380becbe08e953658f0807b3b46ea7927e
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2019-06-06 16:44:21 +0100

    Add code extensions

---
+++ src/test/java/tutorial/TowersOfHanoiTest.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TowersOfHanoiTest {
+
+    @Test
+    public void firstTest() {
+        assertThat(false).isTrue();
+    }
+
+}
--- src/test/java/tutorial/TutorialExercisesTest.java
+++
@@ -1,6 +0,0 @@
-package tutorial;
-
-public class TutorialExercisesTest {
-
-
-}
