784408b611135a44382fd7e1ae02178ccf5828f8
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-06-11 13:58:06 +0100

    Added comments on Tower

--- src/main/java/tutorial/Tower.java
+++ src/main/java/tutorial/Tower.java
@@ -6,20 +6,24 @@ class Tower {
 
     private Stack<Disc> discs = new Stack<>();
 
+    //encapsulation?
     Stack<Disc> getDiscs() {
         return discs;
     }
 
+    //constructor?
     void initialiseDiscsOntoTower(int numberOfDiscs) {
         for (int discSize = numberOfDiscs; discSize > 0; discSize--) {
             discs.push(new Disc(discSize));
         }
     }
 
+    //used?
     int getNumberOfDiscs() {
         return discs.size();
     }
 
+    //naming?
     Disc removedTopDisc() {
         return discs.pop();
     }
